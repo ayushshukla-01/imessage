dadeab7c623ff97e8357c4a2c97cfedff125d50c
ayush shukla <ayushshukla8845@gmail.com>
2026-06-20 18:04:34 +0530

    Fix backend folder

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+//const express = require('express');
+import express from 'express';
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => 
+  console.log("Server is running on port" ));
