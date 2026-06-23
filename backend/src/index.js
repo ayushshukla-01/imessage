1b38a95a5289168e6f99979b23dd388906c7b1ce
ayush shukla <ayushshukla8845@gmail.com>
2026-06-23 18:19:11 +0530

    feat: add messaging functionality with user sidebar, conversation retrieval, and media upload support

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ import {connectDB} from './lib/db.js';
 import job from "./lib/cron.js";
 import clerkWebhook from './webhooks/clerk.webhooks.js';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -30,6 +31,7 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if(fs.existsSync(publicDir)){
   app.use(express.static(publicDir));
