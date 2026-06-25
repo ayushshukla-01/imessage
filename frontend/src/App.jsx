cf84e9c731e510110616a06d14cfe9b4277aff9b
ayush shukla <ayushshukla8845@gmail.com>
2026-06-25 23:20:59 +0530

    feat: add authentication components and theme customization features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,35 @@
-import './App.css';
-import { WallpaperProvider } from './context/WallpaperContext';
-import { ThemeProvider } from './context/ThemeContext';
-import { Navigate, Route, Routes } from 'react-router';
-import ChatPage from './pages/ChatPage';
-import AuthPage from './pages/AuthPage';
-import { useAuth } from '@clerk/react';
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  
+  
+
+  if (!isLoaded) return <PageLoader />;
+    
+
   return (
     <ThemeProvider>
-     <wallpaperProvider>
-       <Routes>
-         <Route path="/" element={<ChatPage />} />
-         <Route path="/auth" element={<AuthPage />} />
-       </Routes>
-      </wallpaperProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+       
+      </WallpaperProvider>
     </ThemeProvider>
   );
 }
 
-export default App
+export default App;
